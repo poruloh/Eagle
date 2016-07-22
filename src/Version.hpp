50d13ff80eb855d5e5bd62e1f810247561dbe792
Po-Ru Loh <loh@hsph.harvard.edu>
2016-07-22 11:47:40 -0400

    updated version + examples to v2.3

--- src/Version.hpp
+++ src/Version.hpp
@@ -19,7 +19,7 @@
 #ifndef VERSION_HPP
 #define VERSION_HPP
 
-const char EAGLE_VERSION[] = "2.2";
-const char EAGLE_VERSION_DATE[] = "July 15, 2016";
+const char EAGLE_VERSION[] = "2.3";
+const char EAGLE_VERSION_DATE[] = "July 22, 2016";
 
 #endif
